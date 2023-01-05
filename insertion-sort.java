c70c6d9d9851e1e096ae75f6a117392832a757cb
Samuel kibret <101621553+samuelkibret@users.noreply.github.com>
2023-01-05 07:13:06 -0800

    Update insertion-sort.java
    the questions main idea is to place the last element at the right place. 
    #compare the last element with all the elements to the left
    #print
    #repeat
    #when the last element is greater than one of the elements
        # print array and stop

--- insertion-sort.java
+++ insertion-sort.java
@@ -1,1 +1,12 @@
-
+int v = arr.get(n-1); // take last number
+            int i = n-2; // get last but one index
+            while(i>=0 && v < arr.get(i)) {  // continue the loop till i>=0 and value < ith value
+                if(arr.get(i) > v)
+                    arr.set(i+1, arr.get(i)); // if value < ith value, shift ith value to i+1 th index
+                i--; // go to left
+                for(int x:arr) System.out.print(x +" "); // print the array in each setp
+                System.out.println();
+            }
+            arr.set(i+1, v); // at ent set i+th value to value selected in the begining
+            for(int x:arr) System.out.print(x +" "); // print the array again
+            System.out.println();
