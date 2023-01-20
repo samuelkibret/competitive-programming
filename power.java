4da7b26263def7a4dce913c93efd286cc70e27c8
Samuel kibret <101621553+samuelkibret@users.noreply.github.com>
2023-01-20 05:01:13 -0800

    Update power.java

--- power.java
+++ power.java
@@ -1,1 +1,29 @@
-
+class Solution {
+  
+    public double myPow(double x, int n) {
+        if(n == 0 )
+        {
+            return 1;
+        }
+        if(x == 0)
+        {
+            return 0;
+        }
+    
+        if(n < 0)
+        {
+      
+          return ((1 / x) * (myPow(1 / x, -(n + 1))));
+        }
+        
+       if(n % 2 == 0)
+       {
+        return myPow(x*x , n / 2);   
+       } 
+       else
+       {
+           return x * myPow(x , n -1);
+       }
+   
+    }
+}
