07c5182e88e41d1227fa51c883a20f03a065f80e
Samuel kibret <101621553+samuelkibret@users.noreply.github.com>
2023-02-08 23:35:33 -0800

    Update reverse_polish_notation.java

--- reverse_polish_notation.java
+++ reverse_polish_notation.java
@@ -1,29 +1,30 @@
 class Solution {
-    // public int evalRPN(String[] tokens) {
-//         Stack <Integer> stack = new Stack<>();
-//         for(String s : tokens)
-//         {
-//             if("+-*/".contains(s))
-//             {
-//                 stack.push(calculate(stack.pop() , stack.pop() , s));
-//             }
-//             else
-//             {
-//                 stack.push(Integer.parseInt(s));
-//             }
+     public int evalRPN(String[] tokens) {
+        Stack <Integer> stack = new Stack<>();
+        for(String s : tokens)
+         {
+             if("+-*/".contains(s))
+             {
+                 stack.push(calculate(stack.pop() , stack.pop() , s));
+             }
+             else
+             {
+                 stack.push(Integer.parseInt(s));
+             }
             
-//         }
-//         return stack.pop();
-//     }
+         }
+         return stack.pop();
+     }
     
-//   private int calculate(int a , int b , String c) 
-//     {
-//           if("+".equals(c))
-//             return a+b;
-//             else if("-".equals(c))
-//             return a-b;
-//             else if("*".equals(c))
-//             return a*b;
-//             else
-//             return a/b;
-//     }
+   private int calculate(int a , int b , String c) 
+     {
+           if("+".equals(c))
+             return a+b;
+             else if("-".equals(c))
+             return a-b;
+             else if("*".equals(c))
+             return a*b;
+             else
+             return a/b;
+     }
+    )
