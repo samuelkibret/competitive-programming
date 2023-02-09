4f0a811864c5b56966a16f6978c001d7e3acffa8
Samuel kibret <101621553+samuelkibret@users.noreply.github.com>
2023-02-08 23:33:47 -0800

    Create reverse_polish_notation.java

---
+++ reverse_polish_notation.java
@@ -0,0 +1,29 @@
+class Solution {
+    // public int evalRPN(String[] tokens) {
+//         Stack <Integer> stack = new Stack<>();
+//         for(String s : tokens)
+//         {
+//             if("+-*/".contains(s))
+//             {
+//                 stack.push(calculate(stack.pop() , stack.pop() , s));
+//             }
+//             else
+//             {
+//                 stack.push(Integer.parseInt(s));
+//             }
+            
+//         }
+//         return stack.pop();
+//     }
+    
+//   private int calculate(int a , int b , String c) 
+//     {
+//           if("+".equals(c))
+//             return a+b;
+//             else if("-".equals(c))
+//             return a-b;
+//             else if("*".equals(c))
+//             return a*b;
+//             else
+//             return a/b;
+//     }
