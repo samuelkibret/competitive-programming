01c993efb1205c6533f1f4ac8efb06dce8d1ae54
Samuel kibret <101621553+samuelkibret@users.noreply.github.com>
2023-02-08 23:46:02 -0800

    Create queue_implementtion.java

---
+++ queue_implementtion.java
@@ -0,0 +1,43 @@
+class MyQueue {
+     private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public MyQueue() {
+        
+    }
+    
+    public void push(int x) {
+        if(stack1.isEmpty()){
+           stack1.push(x);  
+           return;
+    }
+      while(!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+      }
+       stack1.push(x);
+        while(!stack2.empty()){
+             stack1.push(stack2.pop());
+        }
+    }
+    
+    public int pop() {
+        return stack1.pop();
+    }
+    
+    public int peek() {
+        return stack1.peek();
+    }
+    
+    public boolean empty() {
+        return stack1.isEmpty() && stack2.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
